98624b33c181288e63ab5cc71c88981d4916c3d0
Phyllis Atieno <atienophyllis032@gmail.com>
2021-12-16 13:43:47 +0300

    Changed status to working

--- func1.php
+++ func1.php
@@ -2,8 +2,8 @@
 session_start();
 $con=mysqli_connect("localhost","root","","myhmsdb");
 if(isset($_POST['docsub1'])){
-	$dname=$_POST['username3'];
-	$dpass=$_POST['password3'];
+	$dname=$_POST['username'];
+	$dpass=$_POST['password'];
 	$query="select * from doctb where username='$dname' and password='$dpass';";
 	$result=mysqli_query($con,$query);
 	if(mysqli_num_rows($result)==1)
